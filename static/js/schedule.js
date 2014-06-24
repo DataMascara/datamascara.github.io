25e5773dedc35090f10b02a8e81edea1dea934c2
Kat Chuang <katychuang@gmail.com>
2014-06-23 23:59:15 -0400

    update schedule

--- static/js/schedule.js
+++ static/js/schedule.js
@@ -31,7 +31,23 @@ var MySchedule = React.createClass({
                 , topic:"Data Viz Weight Loss"
                 , description: "We'll be looking at several examples of improving presentation of data."
                 , speakers: []
+		}, {
+		  date: "2014-06-16 19:00:00"
+		, topic: "Study Hall"
+		, description: ""
+		, speakers: []
+		}, {
+		  date: "2014-06-22 19:00:00"
+		, topic: "Analyzing rap lyrics"
+		, description: "The topic for this week is about analyzing rap lyrics.. Here are some examples of work we will be looking at: <br /><a href='http://pyvideo.org/video/2658/analyzing-rap-lyrics-with-python'> Analyzing Rap Lyrics with Python</a><br /><a href='http://rappers.mdaniels.com.s3-website-us-east-1.amazonaws.com/'>The Largest Vocabulary in Hip Hop</a>"
+		, speakers: []
+		}, {
+		  date: "2014-06-30 19:00:00"
+		, topic: "Show and tell"
+		, description: "Chris Jenkins, a Ph.D. Candidate in experimental psychology from the University of New Mexico will be describing how he got started with scientific programming packages in Python. He'll show us how he's been using IPython Notebook, pandas, matplotlib, and Bokeh to analyze data from his research on differences in rhythmic timing accuracy."
+		, speakers: []
     }];
+    var n = schedule.length;
     return (
         <div>
             <h2>
@@ -42,10 +58,10 @@ var MySchedule = React.createClass({
             Check the <a href="http://www.meetup.com/nycpython/#calendar">NYC Python Calendar</a> for meeting locations.
             </p>
             <p>
-                <i>{moment(schedule[1].date).fromNow()}</i> - "{schedule[1].topic}"
+                <i>{moment(schedule[n-1].date).fromNow()}</i> - "{schedule[n-1].topic}"
             </p>
             <p>
-                {schedule[1].description}
+                {schedule[n-1].description}
             </p>
         </div>
     );
